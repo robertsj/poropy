2405e6661752bc67e988ae4bbeef9f19baeeb333
Jeremy Roberts <robertsj@mit.edu>
2012-05-17 07:57:26 -0400

    added new test problem to gui (also in examples)

--- poropy/coretools/optimizer_gesbs.py
+++ poropy/coretools/optimizer_gesbs.py
@@ -9,6 +9,17 @@ import numpy as np
 
 class OptimizerGESBS(Optimizer) :
     """  Optimizes using greedy exhaustive single binary sweeps.
+
+    The basic algorithm is as follows.  For each bundle, swap with
+    every other bundle.  Keep track of the single swap that improves
+    the objective most.  For a quarter core of 50 assemblies, this
+    means do 2500 swaps and record the best.  Then, do the swap, 
+    and perform 2500 new swaps, keeping the best.  Do this until
+    converged.
+
+    As an alternative, a flag can be set to make the method
+    greedier: in this case, as soon as a swap improves the objective,
+    it is kept.
     """
     
     # Public Interface
